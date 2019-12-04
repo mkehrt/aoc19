150f7559f975c1834d28a5817028f923abf0c9b4
Matthew Kehrt <mkehrt@roseandsigil.net>
2019-12-04 00:27:06 -0800

    1.2

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,12 +1,23 @@
 use std::{env, fs};
 
+fn fuel_for_mass(mass: i32) -> i32 {
+    let fuel = (mass / 3) - 2;
+    if fuel > 0 {
+        fuel + fuel_for_mass(fuel)
+    } else {
+        0
+    }
+}
+
 fn main() {
-    let args:Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     let input = &args[1];
     let contents = fs::read_to_string(input).unwrap();
 
-    let masses = contents.split_whitespace().map(|x| x.parse::<i32>().unwrap());
-    let fuel: i32 = masses.map(|x| (x / 3) - 2).sum();
-    
+    let masses = contents
+        .split_whitespace()
+        .map(|x| x.parse::<i32>().unwrap());
+    let fuel: i32 = masses.map(fuel_for_mass).sum();
+
     println!("{}", fuel);
 }
